14b661e269bdadb8e5e9ec0ba068a557d0b698a3
Faizan Memon <faizan@sharechat.co>
2023-05-01 11:49:17 +0530

    Initial Commit

---
+++ MainView/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  MainView
+//
+//  Created by Faizan Memon on 01/05/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
